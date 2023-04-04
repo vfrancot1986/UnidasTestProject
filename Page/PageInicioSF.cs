a0abaad89b3b531bc1ecf02b7d06304eea74b27b
Vinicius Franco <vinicius.ts.franco@gmail.com>
2023-04-04 17:37:35 -0300

    oportunidade

--- Page/PageInicioSF.cs
+++ Page/PageInicioSF.cs
@@ -7,6 +7,23 @@ namespace UnidasTestProject.Page
     public class PageInicioSF
     {
 
+        [FindsBy(How = How.XPath, Using = "//button[@class='slds-button slds-icon-waffle_container slds-context-bar__button slds-button forceHeaderButton salesforceIdentityAppLauncherHeader']")]
+        [CacheLookup]
+        private IWebElement _btnSalesConsole;
+
+        [FindsBy(How = How.XPath, Using = "//input[@placeholder='Pesquisar aplicativos e itens…']")]
+        [CacheLookup]
+        private IWebElement _cpPesquisarApp;
+
+        [FindsBy(How = How.XPath, Using = "//button[@title='Mostrar Menu de navegação']")]
+        [CacheLookup]
+        private IWebElement _btnMenuNavegacao;
+
+        [FindsBy(How = How.XPath, Using = "//a[@data-label='Contas']")]
+        [CacheLookup]
+        private IWebElement _optContas;
+
+
         [FindsBy(How = How.XPath, Using = "//*[@id='brandBand_1']//force-list-view-manager-search-bar//div//lightning-input")]
         [CacheLookup]
         private IWebElement _cpPesquisar;
@@ -29,6 +46,12 @@ namespace UnidasTestProject.Page
         //Acoes da pagina
         public void PesquisarConta()
         {
+            TestBase.thisElement(_btnSalesConsole, action.Click);
+            TestBase.thisElement(_cpPesquisarApp, action.Click);
+            TestBase.thisElement(_cpPesquisarApp, action.SendKey, "Sales Console");
+            TestBase.thisElement(_cpPesquisarApp, action.Enter);
+            TestBase.thisElement(_btnMenuNavegacao, action.Click);
+            TestBase.thisElement(_optContas, action.Click);
             TestBase.thisElement(_txtCabecalhoConta, action.Click);
             TestBase.thisElement(_cpPesquisar, action.Click);
             TestBase.thisElement(_cpPesquisar, action.SendKey, "SEMAR SUPERMERCADO LTDA");
